861dad1e5829ad1c540a71560d419be6c925328e
Martin Šnajdr <martin.snajdr@gmail.com>
2026-03-13 01:04:30 +0100

    Initial Commit

---
+++ fotone/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  fotone
+//
+//  Created by Martin Šnajdr on 13.03.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ fotoneTests/fotoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  fotoneTests.swift
+//  fotoneTests
+//
+//  Created by Martin Šnajdr on 13.03.2026.
+//
+
+import Testing
+@testable import fotone
+
+struct fotoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
